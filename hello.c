fa6c1c583a08a2a8d078a9c1707655e57e023a50
tania <tvt9@cornell.edu>
2016-10-12 20:40:09 -0400

    filter no drops all localhost->localhost traffic
    DNS and TCP packets still go through, but they're the standard
    queries and TCP Retransmission packets.

--- hello.c
+++ hello.c
@@ -2,15 +2,46 @@
 #include <linux/kernel.h> /* Needed for KERN_INFO */
 #include <linux/netfilter.h>
 #include <linux/netfilter_ipv4.h>
+#include <linux/ip.h>
+#include <linux/skbuff.h>
+
+#define LOCALHOST 16777343
 
 // struct holding set of hook function options
 static struct nf_hook_ops nfho; 
 
-unsigned int hook_func(unsigned int hooknum, struct sk_buff **skb,
+/* 
+ * RETURNS:
+ *  NF_ACCEPT : let the packet pass
+ *  NF_DROP   : drop the packet
+ *  NF_STOLEN : take the packet and don't let the packet pass
+ *  NF_QUEUE  : queue the packet, usually for userspace
+ *  NF_REPEAT : call the hook again
+ */
+unsigned int hook_func(unsigned int hooknum, struct sk_buff *skb,
                       const struct net_device *in, const struct net_device *out,
                       int (*okfn)(struct sk_buff *)) {
-  printk(KERN_INFO "packet dropped\n");
-  return NF_DROP;
+  unsigned int src_ip;
+  unsigned int dest_ip;
+  struct iphdr *ipheader;
+
+  // skb_mac_header(skb)
+
+  // doesn't always work. "Works when the network packets are processed by
+  // certain functions of netfilter which is at the transport layer of netfilter
+  // implementation"
+  // skb_transport_header(skb)
+
+  // getting IP header
+  ipheader = (struct iphdr *)skb_network_header(skb);
+  src_ip  = (unsigned int) (ipheader->saddr);
+  dest_ip = (unsigned int) (ipheader->daddr);
+  if (src_ip == LOCALHOST && dest_ip == LOCALHOST) {
+    printk(KERN_INFO "packet dropped\n");
+    return NF_DROP;
+  } 
+
+  return NF_ACCEPT;
 }
 
 /* Kernel Modules must have at least the following two functions
